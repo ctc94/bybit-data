4a7787dc214e27a16b46e98f71c02917f73c020c
ctc948040 <mir948040@naver.com>
2021-11-07 01:12:15 +0900

    2021-11-07  1:12:15 modify files

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 sh run_redis.sh
-
-if [ "" = "${JAVA_HOME}" ] ; then
-  export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-fi
+echo "JAVA_HOME_BEFORE=${JAVA_HOME}"
+#if [ "" = "${JAVA_HOME}" ] ; then
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+#fi
 
 echo "JAVA_HOME=${JAVA_HOME}"
 
